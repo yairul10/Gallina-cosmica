cd53b616cfe43c0204b10acca424d15b0c2acf55
yairul10 <jairog940@gmail.com>
2026-09-26 03:15:35 +0000

    Update current app version to 1.4.22

--- js/update-notice.js
+++ js/update-notice.js
@@ -1,6 +1,6 @@
 /* Aviso opcional de actualizaciones. No bloquea el modo offline. */
 (() => {
-    const CURRENT_VERSION = '1.4.21';
+    const CURRENT_VERSION = '1.4.22';
     const VERSION_URL = 'https://yairul10.github.io/Gallina-cosmica/version.json';
 
     const versionParts = (version) => String(version || '')
